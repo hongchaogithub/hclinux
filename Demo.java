1853372ed8408fd7a0905170a1e49246e19e0c89
roy <roy@abc.com>
2018-05-08 15:38:38 +0800

    1538

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
     public static void main(String args[])
     {
-        System.out.println("Hello World second time");
+        System.out.println("Hello World second time for test");
     }
 }
