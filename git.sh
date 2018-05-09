6d6bb5f3c3f59b5d40fed07c844ef5f4ce18b6b1
roy <roy@abc.com>
2018-05-09 17:39:50 +0800

    Wed May  9 17:39:50 CST 2018

--- git.sh
+++ git.sh
@@ -23,7 +23,7 @@ while [ -z $githubuser ]; do read -p "请输入github用户名：" githubuser; d
 while [ -z $githubpw ]; do read -s -p "请输入github密码：" githubpw;echo ""; done
 while [ -z $jenkinspw ]; do read -s -p "请输入jenkins密码：" jenkinspw;echo ""; done
 
-#这是用文件存储用户名和密码的方式，比较不安全
+#这是用文件存储用户名和密码的方式，方便，但比较不安全
 githubuser=`sed -n "1p" /usr/share/jengit.txt`
 githubpw=`sed -n "2p" /usr/share/jengit.txt`
 jenkinspw=`sed -n "3p" /usr/share/jengit.txt`
