cd01119c30fe1d1a4f7f1fb1ba34a766575a0c45
roy <roy@abc.com>
2018-05-09 11:12:36 +0800

    Wed May  9 11:12:36 CST 2018

--- git.sh
+++ git.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+#检查是否输入了参数
+if [ -z $1 ]
+then
+	read -p "请输入github用户名：" $githubuser
+else
+ 	githubuser=$1
+fi
+
+if [ -z $2 ]
+then
+        read -p "请输入github密码：" $githubpw
+else
+        githubpw=$2
+fi
+
+if [ -z $3 ]
+then
+        read -p "请输入jenkins密码：" $jenkinspw
+else
+        jenkinspw=$3
+fi
+
+
 #github的提交操作
 read -p "请输入代码提交信息，如果直接回车将以日期时间作为代码提交信息：" info
 git add .
@@ -12,11 +35,11 @@ fi
 expect <<EOF # > /dev/null 2>&1
 spawn git push origin master
 expect "github.com':"
-send "$1\r\n"
+send "$githubuser\r\n"
 expect "@github.com':"
-send "$2\r\n"
+send "$githubpw\r\n"
 expect eof
 EOF
 
 #远程触发构建
-curl -u admin:$3 -X POST http://182.10.1.55:8080/job/demo/build?token=123456
+curl -u admin:$jenkinspw -X POST http://182.10.1.55:8080/job/demo/build?token=123456
